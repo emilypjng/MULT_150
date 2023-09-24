89214c1355ab59bcd1fb539da154f82b9ef44c34
emilypjng <emilypjng16@gmail.com>
2023-09-23 22:34:45 -0700

    hour 7

---
+++ eng_Hour7/Assets/Scripts/HelloWorldScript.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+
+public class HelloWorldScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print ("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ eng_Hour7/Assets/Scripts/MyBirthday.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyBirthday : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        print ("1 is January");
+        print ("2 is February");
+        print ("3 is March");
+        print ("4 is April");
+        print ("It's my birthday");
+        print ("6 is June");
+        print ("7 is July");
+        print ("8 is August");
+        print ("9 is September");
+        print ("10 is October");
+        print ("11 is November");
+        print ("12 is December");
+
+        print ("1");
+        print ("2");
+        print ("3");
+        print ("4");
+        print ("5");
+        print ("6");
+        print ("7");
+        print ("8");
+        print ("9");
+        print ("10");
+        print ("11");
+        print ("12");
+        print ("13");
+        print ("14");
+        print ("15");
+        print ("It's my birthday!");
+        print ("17");
+        print ("18");
+        print ("19");
+        print ("20");
+        print ("21");
+        print ("22");
+        print ("23");
+        print ("24");
+        print ("25");
+        print ("26");
+        print ("27");
+        print ("28");
+        print ("29");
+        print ("30");
+        print ("31");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
